98ee3b9b56caac5e55915c9cb0a26e21100f36b1
Siavash Ghanbari <sia.qnbr@gmail.com>
2022-11-11 20:00:21 +0100

    Show Correct/Incorrect items :sparkles:

--- App.js
+++ App.js
@@ -5,7 +5,11 @@ import AppButton from './components/AppButton';
 import SOURCE from './questions.json';
 import {
   ITEM_COLOR,
-  ITEM_BACKGROUND_COLOR
+  ITEM_BACKGROUND_COLOR,
+  ITEM_COLOR_CORRECT,
+  ITEM_BACKGROUND_CORRECT,
+  ITEM_COLOR_WRONG,
+  ITEM_BACKGROUND_WRONG
 } from "./common/theme";
 
 const NUMBER_OF_QUESTIONS = 300;
@@ -13,14 +17,39 @@ const NUMBER_OF_QUESTIONS = 300;
 export default function App() {
   const [index, setIndex] = useState(0);
   const item = SOURCE[index];
-  const {question, qid, answers, metadata } = item;
+  const { question, qid, answers, metadata } = item;
+  const { rightAnswer } = metadata;
+  const [status, setStatus] = useState('blank'); // blank, wrong, correct
+  const shouldRenderStatus = status !== "blank";
 
+  const onItemPress = (index) => {
+    if(index === rightAnswer) {
+      setStatus('correct');
+    }
+    else {
+      setStatus('wrong');
+    }
+  };
 
   const onPressNextQuestion =() => {
     const randomIndex = Math.floor(Math.random()* NUMBER_OF_QUESTIONS);
     setIndex(randomIndex);
+    setStatus('blank');
   };
 
+  const getStyle = (index) => {
+    if(status === 'blank') {
+      return [ITEM_COLOR, ITEM_BACKGROUND_COLOR];
+    }
+    
+    if(index === rightAnswer) {
+      return [ITEM_COLOR_CORRECT, ITEM_BACKGROUND_CORRECT];
+    }
+    else{
+      return [ITEM_COLOR_WRONG, ITEM_BACKGROUND_WRONG];
+    }
+
+  }
   
   return (
     <View style={styles.container}>
@@ -29,17 +58,20 @@ export default function App() {
           {answers.map((answer, index) => {
             return (
               <AppButton 
-                color={ITEM_COLOR}
-                backgroundColor={ITEM_BACKGROUND_COLOR}
+                color={getStyle(index)[0]}
+                backgroundColor={getStyle(index)[1]}
                 key={index}
                 title={answer}
                 style={styles.item}
+                onPress={() => onItemPress(index)}
                 />
             )
-          })}
+          })}  
       </View>
 
-      <AppButton title="Next Question" onPress={onPressNextQuestion} style={styles.button} />
+        <AppButton title="Next Question" onPress={onPressNextQuestion} style={styles.button} />
+        {shouldRenderStatus && <View><Text>{status}</Text></View>}
+
     </View>
   );
 }
@@ -47,7 +79,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#990017',
+    backgroundColor: '#b7c072',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 50
@@ -59,7 +91,7 @@ const styles = StyleSheet.create({
     margin: 20
   },
   question : {
-    color: "#e3e334",
+    color: "#371a13",
     padding: "20",
     fontSize: "24px"
   },
@@ -69,6 +101,7 @@ const styles = StyleSheet.create({
   itemList: {
     alignItems: "flex-start",
     justifyContent: "flex-start",
+    marginStart: 10
 
   }
 });
--- common/theme.js
+++ common/theme.js
@@ -4,3 +4,10 @@ export const BUTTON_BACKGROUND_COLOR = '#000';
 
 export const ITEM_COLOR = '#000';
 export const ITEM_BACKGROUND_COLOR = 'rgba(255,255,255,0.5)';
+
+
+export const ITEM_COLOR_CORRECT = '#000';
+export const ITEM_BACKGROUND_CORRECT = 'rgba(0,255,0,0.5)';
+
+export const ITEM_COLOR_WRONG = '#000';
+export const ITEM_BACKGROUND_WRONG = 'rgba(255,0,0,0.5)';
